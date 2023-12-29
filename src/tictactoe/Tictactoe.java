e976021ea1b8a0861a7ed64c37d8209d2f55326f
Hp <Hp@Rafay-PC>
2023-12-29 13:01:03 +0500

    Project

---
+++ src/tictactoe/Tictactoe.java
@@ -0,0 +1,16 @@
+
+package tictactoe;
+
+import java.util.Random;
+
+public class Tictactoe {
+     int board[][] = new int[3][3];
+
+    public static void main(String[] args) {
+       Tictactoe t = new Tictactoe();
+      new startpage(t).setVisible(true);
+        
+        
+    }
+    
+}
